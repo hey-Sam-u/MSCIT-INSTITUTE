c10dee446359b8627352bacb67433a372213d7a8
hey-Sam-u <samirpatil1626@gmail.com>
2025-10-29 09:04:26 +0530

    fixed mysql env variable names

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
 });
 db.connect((err) => {
   if (err) console.log("DB Error:", err);
